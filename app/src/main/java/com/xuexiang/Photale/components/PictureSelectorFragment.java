af5e421afb0caa16adbe5bec73413a2ca5d8834c
昱帆 <42029541+Funoo@users.noreply.github.com>
2021-03-17 15:35:00 +0800

    feat: "Update addTag function and add generate text function"

--- app/src/main/java/com/xuexiang/Photale/components/PictureSelectorFragment.java
+++ app/src/main/java/com/xuexiang/Photale/components/PictureSelectorFragment.java
@@ -32,19 +32,22 @@ import com.luck.picture.lib.config.PictureConfig;
 import com.luck.picture.lib.entity.LocalMedia;
 import com.xuexiang.Photale.core.BaseFragment;
 import com.xuexiang.Photale.utils.Utils;
+import com.xuexiang.Photale.utils.XToastUtils;
 import com.xuexiang.xpage.annotation.Page;
 import com.xuexiang.xui.widget.actionbar.TitleBar;
 
 import java.util.ArrayList;
 import java.util.List;
 import com.xuexiang.Photale.R;
+import com.xuexiang.xui.widget.button.roundbutton.RoundButton;
+import com.xuexiang.xui.widget.edittext.MultiLineEditText;
 import com.xuexiang.xui.widget.edittext.materialedittext.MaterialEditText;
 import com.xuexiang.xui.widget.flowlayout.FlowTagLayout;
 
 import butterknife.BindView;
 import butterknife.OnClick;
 
-@Page(name = "图片选择")
+@Page(name = "文案生成")
 public class PictureSelectorFragment extends BaseFragment implements ImageSelectGridAdapter.OnAddPicClickListener {
 
     @BindView(R.id.recycler_view)
@@ -56,11 +59,21 @@ public class PictureSelectorFragment extends BaseFragment implements ImageSelect
     @BindView(R.id.tagEdit)
     MaterialEditText tagEdit;
 
+    @BindView(R.id.article)
+    MultiLineEditText myarticle;
+
+    @BindView(R.id.generateText)
+    RoundButton generateText;
+
     private ImageSelectGridAdapter mAdapter;
 
     private List<LocalMedia> mSelectList = new ArrayList<>();
 
     private String tagString;
+
+    private int tagListNum = 0;
+
+    private List<String> tagInfo = new ArrayList<>();
 
 
     @SuppressLint("ResourceAsColor")
@@ -96,6 +109,7 @@ public class PictureSelectorFragment extends BaseFragment implements ImageSelect
         Utils.getPictureSelector(this)
                 .selectionMedia(mSelectList)
                 .forResult(PictureConfig.CHOOSE_REQUEST);
+        myarticle.setVisibility(View.GONE);
     }
 
 
@@ -140,16 +154,37 @@ public class PictureSelectorFragment extends BaseFragment implements ImageSelect
     void onClick(View v) {
         switch(v.getId()) {
             case R.id.btn_add_tag:
-                tagString = tagEdit.getText().toString();
-                mDisplayFlowTagLayout.addTag(tagString);
-                tagEdit.clear();
-                System.out.println("--------------addTag-----------------");
+                if (tagEdit.getText().length() == 0){
+                    XToastUtils.toast("请输入标签内容喔~");
+                }else {
+                    tagString = tagEdit.getText().toString();
+                    mDisplayFlowTagLayout.addTag(tagString);
+                    tagInfo.add(tagListNum,tagString);
+                    tagListNum++;
+                    System.out.println("---------" + tagInfo + "-------------" + tagListNum);
+                    tagEdit.clear();
+                }
                 break;
             case R.id.btn_clear_tag:
                 mDisplayFlowTagLayout.clearTags();
+                tagInfo.clear();
+                tagListNum = 0;
+                System.out.println("---------" + tagInfo + "-------------" + tagListNum);
                 break;
             default:
                 break;
+        }
+    }
+
+    @OnClick(R.id.generateText)
+    public void onGenerateClicked() {
+        if (generateText.getText().toString().equals("生成文案")){
+            myarticle.setVisibility(View.VISIBLE);
+            myarticle.setContentText("      我们要学会珍惜我们生活的每一天，因为，这每一天的开始，都将是我们余下生命之中的第一天。除非我们即将死去。");
+            generateText.setText("换个文案");
+        }else {
+            myarticle.setVisibility(View.VISIBLE);
+            myarticle.setContentText("      也许每一个男子全都有过这样的两个女人，至少两个。娶了红玫瑰，久而久之，红的变了墙上的一抹蚊子血，白的还是“床前明月光”；娶了白玫瑰，白的便是衣服上的一粒饭粘子，红的却是心口上的一颗朱砂痣。");
         }
     }
 }
